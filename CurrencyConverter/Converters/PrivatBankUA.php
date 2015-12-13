2c05a2c4be07c4ddf8c852d735e288c2d9989043
toxa <anatoxa@gmail.com>
2015-12-13 22:31:33 +0200

    add privatbank UA converterr

---
+++ CurrencyConverter/Converters/PrivatBankUA.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace CurrencyConverter\Converters;
+
+use \CurrencyConverter\Converter;
+use \CurrencyConverter\ConverterException;
+
+class PrivatBankUA extends Converter
+{
+    /* Different request formats */
+    const PB_FORMAT_JSON = 'json';
+    const PB_FORMAT_XML = 'xml';
+
+    /* Different currency rates */
+    const PB_RATE_CACHE = '5';
+    const PB_RATE_CARDS = '11';
+
+    private static $format = self::PB_FORMAT_JSON;
+    private static $rate = self::PB_RATE_CARDS;
+
+    private $url = 'https://api.privatbank.ua/p24api/pubinfo?exchange&%s&coursid=%s';
+
+
+    public function __construct()
+    {
+        $this->url = sprintf(
+           $this->url,
+           self::$format,
+           self::$rate
+           );
+    }
+
+
+    public function rate($from, $to)
+    {
+        $rate = null;
+
+        $responseArray = $this->send();
+
+        if (empty($responseArray) || !is_array($responseArray)) {
+            throw new ConverterException('Wrong API response structure');
+        }
+        $currencyRates = $this->makeCurrencyRates($responseArray);
+        if (array_key_exists($from, $currencyRates)
+           && array_key_exists($to, $currencyRates[$from])
+        ) {
+            $rate = $currencyRates[$from][$to];
+        } else {
+            throw new ConverterException("Can not find rate for {$from} to {$to}");
+        }
+
+
+        return $rate;
+    }
+
+    private function send()
+    {
+
+        $response = file_get_contents($this->url);
+        if (empty($response)) {
+            throw new ConverterException("API connection failed");
+        }
+
+        return json_decode($response, true);
+    }
+
+    private function makeCurrencyRates(Array $responseArray)
+    {
+        $currencyRates = array();
+
+        foreach ($responseArray as $oneRate) {
+            if (!array_key_exists($oneRate['base_ccy'], $currencyRates)) {
+                $currencyRates[$oneRate['base_ccy']] = array(
+                   $oneRate['base_ccy'] => 1,
+                );
+            }
+            if (!array_key_exists($oneRate['ccy'], $currencyRates)) {
+                $currencyRates[$oneRate['ccy']] = array(
+                   $oneRate['ccy'] => 1,
+                );
+            }
+            $currencyRates[$oneRate['base_ccy']][$oneRate['ccy']] = $oneRate['buy'];
+            $currencyRates[$oneRate['ccy']][$oneRate['base_ccy']] = $oneRate['sale'];
+        }
+
+        return $currencyRates;
+    }
+
+}
