e4697788168c12d24cfd74abf7ba9582feeb5cd2
zhikiri <zhivitsa.kirill.gm@gmail.com>
2015-12-16 21:21:00 +0200

    Changed the Free provider class according to the new API version Initialized project as the composer project

--- CurrencyConverter/Converters/Free.php
+++ CurrencyConverter/Converters/Free.php
@@ -8,25 +8,49 @@ use CurrencyConverter\ConverterException;
 class Free extends Converter
 {
 
-    public function rate($from, $to)
-    {
-        $key = $from."_".$to;
-        $response = $this->send("convert", ["q" => $key, "compact" => "y"]);
-        if (!is_array($response) && !array_key_exists($key, $response)) {
-            throw new ConverterException("Wrong API response structure");
-        }
+    // NOTICE: Put here your API key
+    const API_KEY = null;
 
-        return $response[$key];
+    private $current_api_key;
+
+    public function __construct($api_key = self::API_KEY)
+    {
+        $this->current_api_key = $api_key;
     }
 
-    private function send($operation, $query)
+    public function rate($from, $to)
     {
-        $response = file_get_contents(sprintf(
-            "http://www.freecurrencyconverterapi.com/api/v2/%s?%s",
-            $operation, http_build_query($query)
-        ));
+        $response = $this->send(
+            'covert',
+            array(
+                'from' => $from,
+                'to' => $to,
+                'amount' => 1,
+                'format' => 1,
+            )
+        );
+        if (!is_array($response) || !isset($response['result'])) {
+            throw new ConverterException('Invalid API response');
+        }
+
+        return $response['result'];
+    }
+
+    private function send($operation, array $query)
+    {
+        $endpoint = sprintf(
+            'https://apilayer.net/api/%s?%s',
+            $operation,
+            http_build_query(
+                array_merge(
+                    $query,
+                    array('access_key' => $this->current_api_key)
+                )
+            )
+        );
+        $response = file_get_contents($endpoint);
         if (empty($response)) {
-            throw new ConverterException("API connection failed");
+            throw new ConverterException('Exchange provider connection error');
         }
 
         return json_decode($response, true);
